777583dae7f3b5424e3df2e635fd7141ed902c86
Sayantan Mandal <83362170+SayantanMandal2000@users.noreply.github.com>
2022-04-11 23:12:21 +0530

    Update DHT_LCD.ino

--- Arduino Test Code/DHT11 Sensor/DHT_LCD.ino
+++ Arduino Test Code/DHT11 Sensor/DHT_LCD.ino
@@ -1,26 +1,31 @@
+#include <LiquidCrystal.h>	// Include LiquidCrystal Library
 #include <dht.h>
-#include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#define outPin 8
 
-dht DHT;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // Create an LCD object.
+dht DHT;		// Create a DHT object
 
-#define DHT11_PIN 7
-
-void setup(){
-  lcd.begin(16, 2);
+void setup() {
+	lcd.begin(16,2); // Initialize the LCD
 }
 
-void loop(){
-  int chk = DHT.read11(DHT11_PIN);
-  lcd.setCursor(0,0); 
-  lcd.print("Temp: ");
-  lcd.print(DHT.temperature);
-  lcd.print((char)223);
-  lcd.print("C");
-  lcd.setCursor(0,1);
-  lcd.print("Humidity: ");
-  lcd.print(DHT.humidity);
-  lcd.print("%");
-  delay(1000);
+void loop() {
+	int readData = DHT.read11(outPin);
+	
+	float t = DHT.temperature;
+	float h = DHT.humidity;
+	
+	lcd.setCursor(0,0);
+	lcd.print("Temp.: ");
+	lcd.print(t);
+	lcd.print((char)223);//shows degrees character
+	lcd.print("C");
+
+	lcd.setCursor(0,1);
+	lcd.print("Humi.: ");
+	lcd.print(h);
+	lcd.print("%");
+	
+	delay(2000);
 }
