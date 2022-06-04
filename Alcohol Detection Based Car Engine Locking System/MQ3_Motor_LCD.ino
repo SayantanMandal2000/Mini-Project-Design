d05db78018352e2e52c13fc43aa1f7999e733123
Sayantan Mandal <83362170+SayantanMandal2000@users.noreply.github.com>
2022-06-04 11:16:47 +0530

    Update MQ3_Motor_LCD.ino

--- Alcohol Detection Based Car Engine Locking System/MQ3_Motor_LCD.ino
+++ Alcohol Detection Based Car Engine Locking System/MQ3_Motor_LCD.ino
@@ -1,24 +1,26 @@
 #include <LiquidCrystal.h> 
 
 #define sensorDigital 10
-#define LED 13
-#define buzzer 12
+#define buzzer 13
 #define sensorAnalog A1
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 int motor1pin1 = 6;
 int motor1pin2 = 7;
+int motor2pin1 = 8;
+int motor2pin2 = 9;
 
 void setup() 
 {
   pinMode(sensorDigital, INPUT);
-  pinMode(LED, OUTPUT);
   pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
 
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
 
   lcd.begin(16,2); 
   lcd.setCursor(0,0);
@@ -41,11 +43,12 @@ void loop()
 
   if (digital == 0) 
   {
-    digitalWrite(LED, HIGH);
     digitalWrite(buzzer, HIGH);
 
     digitalWrite(motor1pin1, LOW);
     digitalWrite(motor1pin2, LOW);
+    digitalWrite(motor2pin1, LOW);
+    digitalWrite(motor2pin2, LOW)
 
     lcd.setCursor(0,0);
     lcd.print("Alcohol Detected");
@@ -57,14 +60,17 @@ void loop()
   
   else if(digital == 1)
   {
-    digitalWrite(LED, LOW);
     digitalWrite(buzzer, LOW);
 
     digitalWrite(motor1pin1, HIGH);
     digitalWrite(motor1pin2, LOW);
+    digitalWrite(motor2pin1, HIGH);
+    digitalWrite(motor2pin2, LOW);
     
     lcd.setCursor(0,0);
     lcd.print("No Alcohol");
+    lcd.setCursor(1,1);
+    lcd.print("Detected");
   
   }
   
